fd44e324e51f1d80a45ed0f6211b1e2214af0079
Athan Johnson <71606525+Athan-Johnson@users.noreply.github.com>
2024-09-21 16:22:40 -0400

    Complete Thompson sampling agent

--- HW1/Assignment 1/bandits/thompsonAgent.py
+++ HW1/Assignment 1/bandits/thompsonAgent.py
@@ -7,9 +7,27 @@ import random
 import argparse
 
 class thompsonAgent: 
-    def __init__(self):
+    def __init__(self, bandit):
         self.name = "Terry the Thompson Sampling Agent"
+        
+        # code I added
+        self.armRewards = [1] * bandit.getNumArms() # these must be initialized to one
+        self.armFails = [1] * bandit.getNumArms()
+        # ends here
     
     def recommendArm(self, bandit, history):
         #Hey, your code goes here!
+        
+        if history:
+            if history[-1][1] == 1:
+                self.armRewards[history[-1][0]] += 1
+            else:
+                self.armFails[history[-1][0]] += 1
+                
+        # get a random value from each distribution
+        samples = [0] * bandit.getNumArms()
+        samples = np.random.beta(self.armRewards, self.armFails)
+        
+        return np.argmax(samples)
+        
         return False
