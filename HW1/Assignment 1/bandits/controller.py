18b94131f631cceff950a09c0a84a78a9d2352dc
Athan Johnson <71606525+Athan-Johnson@users.noreply.github.com>
2024-09-22 11:48:14 -0400

    add --all option for running all agents

--- HW1/Assignment 1/bandits/controller.py
+++ HW1/Assignment 1/bandits/controller.py
@@ -45,6 +45,9 @@ parser = argparse.ArgumentParser(description='Define bandit problem and agents.'
 parser.add_argument('--input', choices=['input/test0.txt', 'input/test1.txt'], default='input/test1.txt', help='The input file, can be input/test0.txt or input/test1.txt')
 parser.add_argument('--agent', choices=AGENTS_MAP.keys(), default='randomAgent', help='The bandit AI. Can be randomAgent, epsGreedyAgent, UCBAgent, or thompsonAgent')
 parser.add_argument('--num_plays', type=int, default = 10000, help='The number of pulls an agent has.')
+# code I added
+parser.add_argument('--all', action='store_true', help='runs all agents and graphs them against each other.')
+# ends here
 args = parser.parse_args()
 
 testBandit = bandit(args.input)
@@ -58,26 +61,53 @@ test0data = [0.5, 0.6, 0.4, 0.3, 0.45, 0.7, 0.65, 0.43, 0.55, 0.45]
 test1data = [0.2, 0.2, 0.2, 0.2, 0.9, 0.2, 0.2, 0.2, 0.2, 0.2]
 # ends here
 
+if not args.all:
+    for numRuns in range(args.num_plays):
+        testArm = agent.recommendArm(testBandit, history)
+        reward = testBandit.pull_arm(testArm)
+        cumulative_reward += reward
+        history.append((testArm, reward))
 
-for numRuns in range(args.num_plays):
-    testArm = agent.recommendArm(testBandit, history)
-    reward = testBandit.pull_arm(testArm)
-    cumulative_reward += reward
-    history.append((testArm, reward))
+        # code I added
+        if args.input == 'input/test0.txt':
+            cumulative_regret.append(cumulative_regret[-1] + 0.7 - test0data[testArm])
+        else:
+            cumulative_regret.append(cumulative_regret[-1] + 0.9 - test1data[testArm])
+        #ends here
 
     # code I added
-    if args.input == 'input/test0.txt':
-        cumulative_regret.append(cumulative_regret[-1] + 0.7 - test0data[testArm])
-    else:
-        cumulative_regret.append(cumulative_regret[-1] + 0.9 - test1data[testArm])
-    #ends here
+    range = range(len(cumulative_regret))
 
-# code I added
-range = range(len(cumulative_regret))
+    plt.plot(range, cumulative_regret)
+    plt.show()
+    # ends here
 
-plt.plot(range, cumulative_regret)
-plt.show()
-# ends here
+    print(cumulative_reward)
+        
+else:
+    for a in AGENTS_MAP:
+        agent = AGENTS_MAP[a](testBandit)
+        history = []
+        cumulative_reward = 0
+        cumulative_regret = [0]
+        
+        for numRuns in range(args.num_plays):
+            testArm = agent.recommendArm(testBandit, history)
+            reward = testBandit.pull_arm(testArm)
+            cumulative_reward += reward
+            history.append((testArm, reward))
 
-print(cumulative_reward)
-    
+            # code I added
+            if args.input == 'input/test0.txt':
+                cumulative_regret.append(cumulative_regret[-1] + 0.7 - test0data[testArm])
+            else:
+                cumulative_regret.append(cumulative_regret[-1] + 0.9 - test1data[testArm])
+            #ends here
+
+        # code I added
+        regretRange = range(len(cumulative_regret))
+
+        plt.plot(regretRange, cumulative_regret, label=a)
+        print(a)
+    plt.legend()
+    plt.show()
