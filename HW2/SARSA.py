efb425d2be1378bbf2cac1dc6273097ce38dbe1c
Athan Johnson <71606525+Athan-Johnson@users.noreply.github.com>
2024-10-24 10:55:28 -0400

    implement most of SARSA, doesn't win games yet

--- HW2/SARSA.py
+++ HW2/SARSA.py
@@ -2,6 +2,7 @@ import gymnasium as gym
 import random
 import tqdm
 import argparse
+import numpy as np
 
 
 # Parsing
@@ -15,24 +16,50 @@ args = parser.parse_args()
 # Create the 4x4 Frozen Lake environment
 env = gym.make("FrozenLake-v1", map_name="4x4", is_slippery=True)
 
+# Initialize parameters
+learning_rate = 0.1
+discount_factor = 0.99
+epsilon = 1.0
+min_epsilon = 0.1
+epsilon_decay = 0.995
+num_actions = env.action_space.n
+num_states = env.observation_space.n
+
+# Initialize Q-table with zeros
+Q = np.zeros((num_states, num_actions))
 
 # this is the for loop we're going to be running the training in
 for i in tqdm.tqdm(range(int(args.iterations))):
 	# Reset the environment to start a new episode
-	# the format here for our dict "policy" is obs, or the number assigned
-	# to the state we're in, and the move from that state
-	obs, info = env.reset()
-	prevObs = obs
+	state, info = env.reset()
+
+	if random.uniform(0, 1) < epsilon:
+		action = env.action_space.sample()
+	else:
+		action = np.argmax(Q[state, :])
 
 	done = False
 	while not done:
 
-		action = env.action_space.sample()
-
 		# Step the environment with the chosen action
-		obs, reward, done, truncated, info = env.step(action)
+		next_state, reward, done, truncated, info = env.step(action)
+
+		if random.uniform(0, 1) < epsilon:
+			next_action = env.action_space.sample()
+		else:
+			next_action = np.argmax(Q[next_state, :])
+
+		Q[state, action] += learning_rate * (reward + discount_factor * Q[next_state, next_action] - Q[state, action])
+
+		state = next_state
+		action = next_action
+
+		# Decay epsilon to reduce exploration over time
+		epsilon = max(min_epsilon, epsilon * epsilon_decay)
+
 		# Print information about the current step
 		# print(f"Obs: {obs}, Reward: {reward}, Done: {done}, Info: {info}")
+
 
 # Close the environment when finished
 env.close()
@@ -51,7 +78,7 @@ for i in tqdm.tqdm(range(games)):
 	done = False
 	while not done:
 		# Implenent the policy
-		action = env.action_space.sample()
+		action = np.argmax(Q[next_state, :])
 
 		# Step the environment with the chosen action
 		obs, reward, done, truncated, info = env.step(action)
@@ -83,7 +110,7 @@ if args.show_final_policy:
 			env.render()
 
 			# Implement the policy
-			action = env.action_space.sample()
+			action = np.argmax(Q[next_state, :])
 
 			# Step the environment with the chosen action
 			obs, reward, done, truncated, info = env.step(action)
